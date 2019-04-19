5ba5563e6045e992112e4d7e4ac1d10398fc65b9
dylanbaker21 <dylan.j.baker@hotmail.com>
2019-04-18 21:48:43 -0400

    initial

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 
 export class TodoItem extends Component {
   getStyle = () => {
     return {
       background: "#f4f4f4",
       padding: "10px",
-      borderBottom: "1px #ccc dotted",
-      textDecoration: this.props.todo.completed ? "line-through" : "none"
+      borderBottom: "1px #ccc dotted"
     };
   };
 
@@ -15,42 +13,16 @@ export class TodoItem extends Component {
     return (
       <div style={this.getStyle()}>
         <p>
-          <input
-            checked={this.props.todo.completed}
-            type="checkbox"
-            onChange={this.props.markComplete.bind(this, this.props.todo.id)}
-            style={{ padding: "55px" }}
-          />
-          {"              "}
-          {this.props.todo.title}
-          <button
-            style={btnStyle}
-            onClick={this.props.delTodo.bind(this, this.props.todo.id)}
-          >
-            Delete
-          </button>
+          {"              First name:\n"}
+          {this.props.todo.name.first}
+          {"              Last name:\n"}
+          {this.props.todo.name.last}
+          {"              Balance:\n"}
+          {this.props.todo.balance}
         </p>
       </div>
     );
   }
 }
-
-// PropTypes
-TodoItem.propTypes = {
-  todo: PropTypes.object.isRequired,
-  markComplete: PropTypes.func.isRequired,
-  delTodo: PropTypes.func.isRequired
-};
-
-const btnStyle = {
-  background: "#ff0000",
-  color: "#fff",
-  border: "none",
-  padding: "5px 10px",
-  margin: "0px 0px 0px 20px",
-  borderRadius: "10%",
-  cursor: "pointer",
-  float: "middle"
-};
 
 export default TodoItem;
